bc35c65dd0f122f950dc62cae81f3b42fb8c6b8c
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-05-28 14:56:44 -0300

    Adicionando colisão ao jogo.

--- ator.js
+++ ator.js
@@ -1,5 +1,6 @@
 let xAtor = 100;
 let yAtor = 366;
+let colisao = false;
 
 function mostraAtor() {
     image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -12,4 +13,21 @@ function movimentaAtor() {
     if (keyIsDown(DOWN_ARROW)) {
         yAtor += 3;
     }
+}
+
+function verificaColisao() {
+
+    for (let i = 0; i < imagemCarros.length; i++) {
+
+        colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
+
+        if (colisao) {
+            colidiu();
+        }
+
+    }
+}
+
+function colidiu() {
+    yAtor = 366;
 }
--- carro.js
+++ carro.js
@@ -3,17 +3,12 @@ let xPosicaoInicial = 600;
 let xCarros = [xPosicaoInicial, xPosicaoInicial, xPosicaoInicial];
 let yCarros = [40, 96, 150];
 let velocidadeCarros = [2, 2.5, 3.2];
-
-let xCarro01 = xPosicaoInicial;
-
-let xCarro02 = xPosicaoInicial;
-
-let xCarro03 = xPosicaoInicial;
-
+let comprimentoCarro = 50;
+let alturaCarro = 40;
 
 function mostraCarro() {
     for (let i = 0; i < imagemCarros.length; i++) {
-        image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40);
+        image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
     }
 }
 
