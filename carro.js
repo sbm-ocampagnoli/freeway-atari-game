d7b344f05d0bd8cba257b5300011aff4a66afd01
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-05-28 10:18:07 -0300

    Adicionando Carros para sempre.

--- carro.js
+++ carro.js
@@ -1,23 +1,38 @@
-let xCarro01 = 600;
+let xPosicaoInicial = 600;
+
+let xCarro01 = xPosicaoInicial;
 let yCarro01 = 40;
 let velocidadeCarro01 = 2;
 
-let xCarro2 = 600;
-let yCarro2 = 96;
+let xCarro02 = xPosicaoInicial;
+let yCarro02 = 96;
 let velocidadeCarro02 = 2.5;
 
-let xCarro3 = 600;
-let yCarro3 = 150;
+let xCarro03 = xPosicaoInicial;
+let yCarro03 = 150;
 let velocidadeCarro03 = 3.2;
+
 
 function mostraCarro() {
     image(imagemCarro01, xCarro01, yCarro01, 50, 40);
-    image(imagemCarro02, xCarro2, yCarro2, 50, 40);
-    image(imagemCarro03, xCarro3, yCarro3, 50, 40);
+    image(imagemCarro02, xCarro02, yCarro02, 50, 40);
+    image(imagemCarro03, xCarro03, yCarro03, 50, 40);
 }
 
 function movimentaCarro() {
     xCarro01 -= velocidadeCarro01;
-    xCarro2 -= velocidadeCarro02;
-    xCarro3 -= velocidadeCarro03;
+    xCarro02 -= velocidadeCarro02;
+    xCarro03 -= velocidadeCarro03;
+}
+
+function voltaPosicaoInicialDoCarro() {
+    if (xCarro01 < -50) {
+        xCarro01 = xPosicaoInicial;
+    }
+    if (xCarro02 < -50) {
+        xCarro02 = xPosicaoInicial;
+    }
+    if (xCarro03 < -50) {
+        xCarro03 = xPosicaoInicial;
+    }
 }
