7c0963083adfc742d1a22f1ec4c1d0bca789b1e1
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-05-28 10:00:04 -0300

    Adicionando mais carros.

--- carro.js
+++ carro.js
@@ -1,10 +1,23 @@
-let xCarro = 600;
-let yCarro = 40;
+let xCarro01 = 600;
+let yCarro01 = 40;
+let velocidadeCarro01 = 2;
+
+let xCarro2 = 600;
+let yCarro2 = 96;
+let velocidadeCarro02 = 2.5;
+
+let xCarro3 = 600;
+let yCarro3 = 150;
+let velocidadeCarro03 = 3.2;
 
 function mostraCarro() {
-    image(imagemCarro, xCarro, yCarro, 50, 40);
+    image(imagemCarro01, xCarro01, yCarro01, 50, 40);
+    image(imagemCarro02, xCarro2, yCarro2, 50, 40);
+    image(imagemCarro03, xCarro3, yCarro3, 50, 40);
 }
 
 function movimentaCarro() {
-    xCarro -= 2;
+    xCarro01 -= velocidadeCarro01;
+    xCarro2 -= velocidadeCarro02;
+    xCarro3 -= velocidadeCarro03;
 }
--- imagens.js
+++ imagens.js
@@ -1,9 +1,13 @@
 let imagemDaEstrada;
 let imagemDoAtor;
-let imagemCarro;
+let imagemCarro01;
+let imagemCarro02;
+let imagemCarro03;
 
 function preload() {
     imagemDaEstrada = loadImage("./imagens/estrada.png");
     imagemDoAtor = loadImage("./imagens/ator-1.png");
-    imagemCarro = loadImage("./imagens/carro-1.png");
+    imagemCarro01 = loadImage("./imagens/carro-1.png");
+    imagemCarro02 = loadImage("./imagens/carro-2.png");
+    imagemCarro03 = loadImage("./imagens/carro-3.png");
 }
